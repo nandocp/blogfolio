f5b662052d633e7581a2369a97898f7892a5b044
Fernando Caçador <fernando.cacador@proton.me>
2026-02-20 17:10:26 -0300

    Added model tests

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_one(:entry).touch(true) }
+  it { should define_enum_for(:status) }
+
+  it 'Should respect :default_scope rule' do
+    create_list(:blog_entry, 20)
+    expect(Blog.order(reviewed_at: :desc)).to eq(Blog.all)
+  end
 end
--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Portfolio, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_one(:entry).touch(true) }
+  it { should define_enum_for(:contract_type) }
+
+  it 'Should respect :default_scope rule' do
+    create_list(:portfolio_entry, 20)
+    expect(Portfolio.order(designed_at: :desc)).to eq(Portfolio.all)
+  end
 end
