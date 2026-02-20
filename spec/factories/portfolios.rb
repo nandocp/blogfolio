373608aa5505d2cd3b3c0428b9219d49b8a506c1
Fernando Caçador <fernando.cacador@proton.me>
2026-02-20 11:39:54 -0300

    Updated attribtues with Faker gem data

--- spec/factories/portfolios.rb
+++ spec/factories/portfolios.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :portfolio do
-    designed_at { "2026-02-19 19:41:40" }
-    company_name { "MyString" }
-    contract_type { 1 }
+    entry { association :entry }
+    designed_at { Faker::Date.between(from: 2.years.ago, to: Date.today) }
+    company_name { Faker::Company.name }
+    contract_type { Portfolio.contract_types.keys.sample }
   end
 end
