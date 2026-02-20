3fc351980b16b202b00ab2d1dbd4a22b379c253b
Fernando Caçador <fernando.cacador@proton.me>
2026-02-20 11:57:36 -0300

    Added default_scope

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,3 @@
 class Blog < ApplicationRecord
+  default_scope { order(reviewed_at: :desc) }
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,7 @@
 class Portfolio < ApplicationRecord
   include Entryable
 
+  default_scope { order(designed_at: :desc) }
+
   enum :contract_type, %i[ freelance contract ]
 end
