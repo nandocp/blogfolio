b32e04a091f10310408631ec0980ab8aebde137f
Fernando Caçador <fernando.cacador@proton.me>
2026-02-20 16:52:44 -0300

    Created test for blogs controller

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -18,7 +18,28 @@ RSpec.describe "Pages", type: :request do
   end
 
   describe "GET /blog" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:all) do
+      create_list(:blog_entry, 15)
+      get blog_path
+    end
+
+    it 'Should return status 200 OK' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'Should be processed by the correct action' do
+      expect(@controller.action_name).to eq('blog')
+    end
+
+    it 'Should return the correct view' do
+      expect(response.body).to include("id='blog-posts'")
+    end
+
+    it 'Should return the correct number of entries' do
+      _test = @controller.instance_variable_get(:@blog_entries)
+      expect(_test.length).to eq(15)
+      expect(_test.all? { |entry| entry.is_a?(Blog) }).to be_truthy
+    end
   end
 
   describe "GET /portfolio" do
