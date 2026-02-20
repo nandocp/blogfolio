ae31b8d83ba5bd14d0abc377b8f6cec73f4a3c46
Fernando Caçador <fernando.cacador@proton.me>
2026-02-20 16:53:17 -0300

    Fixed factory

--- spec/factories/entries.rb
+++ spec/factories/entries.rb
@@ -3,12 +3,15 @@ FactoryBot.define do
     user { association :user }
     title { Faker::Lorem.sentence }
     text { Faker::Lorem.paragraph_by_chars(number: 1_000) }
+    subtitle { Faker::Lorem.paragraph_by_chars(number: 70) }
+    description { Faker::Lorem.paragraph_by_chars(number: 120) }
 
-    trait :seo do
-      subtitle { Faker::Lorem.paragraph_by_chars(number: 70) }
-      description { Faker::Lorem.paragraph_by_chars(number: 120) }
+    factory :blog_entry do
+      entryable { association :blog }
+    end
+
+    factory :portfolio_entry do
+      entryable { association :portfolio }
     end
   end
-
-  factory :entry_with_seo, traits: [ :seo ]
 end
--- spec/factories/portfolios.rb
+++ spec/factories/portfolios.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :portfolio do
-    entry { association :entry }
     designed_at { Faker::Date.between(from: 2.years.ago, to: Date.today) }
     company_name { Faker::Company.name }
     contract_type { Portfolio.contract_types.keys.sample }
