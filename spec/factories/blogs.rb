5af8cc69fbfd41cf161c1920a1ebf07d3b26afcd
Fernando Caçador <fernando.cacador@proton.me>
2026-02-20 11:39:34 -0300

    Set new trait

--- spec/factories/blogs.rb
+++ spec/factories/blogs.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
-  factory :blog do; end
-
-  trait :reviewed do
-    reviewed_by { association :user }
-    reviewed_at { DateTime.now }
+  factory :blog do
+    trait :reviewed do
+      reviewed_by { association :user }
+      reviewed_at { DateTime.now }
+    end
   end
 end
