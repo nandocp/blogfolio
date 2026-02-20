3bc05ae63ce293edd2f01b91920b9658c542cb43
Fernando Caçador <fernando.cacador@proton.me>
2026-02-20 09:11:07 -0300

    Fixed rubocop offenses

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -5,12 +5,12 @@ Clearance.configure do |config|
   config.allow_password_reset = false
   config.cookie_domain = 'blogfolio'
   config.cookie_expiration = lambda { |cookies| 8.hours.from_now.utc }
-  config.cookie_name = "remember_token"
-  config.cookie_path = "/"
+  config.cookie_name = 'remember_token'
+  config.cookie_path = '/'
   config.routes = false
   config.httponly = true
   config.password_strategy = Clearance::PasswordStrategies::BCrypt
-  config.redirect_url = "/"
+  config.redirect_url = '/'
   config.url_after_destroy = nil
   config.url_after_denied_access_when_signed_out = nil
   config.rotate_csrf_on_sign_in = true
@@ -18,7 +18,7 @@ Clearance.configure do |config|
   config.secure_cookie = Rails.configuration.force_ssl
   config.signed_cookie = false
   config.sign_in_guards = []
-  config.user_model = "User"
-  config.parent_controller = "ApplicationController"
+  config.user_model = 'User'
+  config.parent_controller = 'ApplicationController'
   config.sign_in_on_password_reset = false
 end
