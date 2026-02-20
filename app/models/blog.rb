905da5241304ccd8d3d050353b6a815ccef37919
Fernando Caçador <fernando.cacador@proton.me>
2026-02-20 17:09:49 -0300

    Created enum for :status

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,12 @@ class Blog < ApplicationRecord
   include Entryable
 
   default_scope { order(reviewed_at: :desc) }
+
+  enum :status, {
+    draft: 0,
+    written: 1,
+    reviewing: 2,
+    reviewed: 3,
+    published: 4
+  }
 end
