b1757b1cc0c693fbb055a235324d515b0a8130ec
Fernando Caçador <fernando.cacador@proton.me>
2026-02-20 11:03:26 -0300

    Added needed routes tests

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
-  describe "GET /index" do
+  describe "GET /about" do
+    before { get about_path }
+
+    it 'SHould return status 200 OK' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET /blog" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+
+  describe "GET /portfolio" do
     pending "add some examples (or delete) #{__FILE__}"
   end
 end
